c0383c666213d962ea7612dd03c1a786396703b5
Andy Jessica <andyjessica22@gmail.com>
2022-06-14 02:33:18 +0100

    solution

---
+++ src/app.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+
+def prime(s):
+    # your code goes here 
+    s = int(s)
+    if s > 1:
+        for i in range(2, s):
+            if (s % i) == 0:
+                print(s, "is a not a prime number")
+                break
+            else:
+                print(s, "is a prime number")     
+    else:
+        print("not prime")
+    
+   
+def solution(s):
+    return prime(s)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        sys.exit(os.error("Argument required"))
+
+    print(solution(sys.argv[1]))
